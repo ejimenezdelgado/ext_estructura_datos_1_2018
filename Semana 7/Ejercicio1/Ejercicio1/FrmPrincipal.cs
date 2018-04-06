dbcd39b9ee0ad91eb3dc85d5073e9563dc714cf2
Efren <ejimenezdelgado@gmail.com>
2018-04-05 20:27:10 -0600

    Clase 7

--- Semana 7/Ejercicio1/Ejercicio1/FrmPrincipal.cs
+++ Semana 7/Ejercicio1/Ejercicio1/FrmPrincipal.cs
@@ -10,11 +10,28 @@ using System.Windows.Forms;
 
 namespace Ejercicio1
 {
-    public partial class Form1 : Form
+    public partial class FrmPrincipal : Form
     {
-        public Form1()
+        private int[] arregloNumeros;
+        public FrmPrincipal()
         {
             InitializeComponent();
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < arregloNumeros.Length; i++)
+            {
+                if (arregloNumeros[i] == null)
+                {
+                    arregloNumeros[i] = Convert.ToInt32(txtNumero.Value);
+                }
+            }
+        }
+
+        private void btnIniciar_Click(object sender, EventArgs e)
+        {
+            arregloNumeros=new int[Convert.ToInt32(txtIniciador.Value)];
         }
     }
 }
