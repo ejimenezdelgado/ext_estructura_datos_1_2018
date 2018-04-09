7bb00cb7d34653c0a05ff9d7ecce9d34bf3c16df
Efren <ejimenezdelgado@gmail.com>
2018-04-09 16:37:35 -0600

    clase 7

--- Semana 7/Ejercicio1/Ejercicio1/FrmPrincipal.cs
+++ Semana 7/Ejercicio1/Ejercicio1/FrmPrincipal.cs
@@ -22,16 +22,43 @@ namespace Ejercicio1
         {
             for (int i = 0; i < arregloNumeros.Length; i++)
             {
-                if (arregloNumeros[i] == null)
+                if (arregloNumeros[i] == 0)
                 {
                     arregloNumeros[i] = Convert.ToInt32(txtNumero.Value);
+                    break;
                 }
             }
+            txtImpresionNumero.Items.Clear();
+            int suma = 0;
+            int promedio = 0;
+            int max = 0;
+            int min = arregloNumeros[0];
+            for (int i = 0; i < arregloNumeros.Length; i++)
+            {
+                suma = suma + arregloNumeros[i];
+                if (arregloNumeros[i]>max)
+                {
+                    max = arregloNumeros[i];
+                }
+                if (arregloNumeros[i] != 0)
+                {
+                    if (arregloNumeros[i] < min)
+                    {
+                        min = arregloNumeros[i];
+                    }
+                }
+                txtImpresionNumero.Items.Add(arregloNumeros[i]);
+            }
+            txtSuma.Text = suma.ToString();
+            txtPromedio.Text = (suma/arregloNumeros.Length).ToString();
+            txtMaximo.Text = max.ToString();
+            txtMinimo.Text = min.ToString();
         }
 
         private void btnIniciar_Click(object sender, EventArgs e)
         {
             arregloNumeros=new int[Convert.ToInt32(txtIniciador.Value)];
         }
+
     }
 }
