fdccd1872822d4a00d2a67e5bf7d9a2e520e3944
Efren <ejimenezdelgado@gmail.com>
2018-05-17 18:53:55 -0600

    clase 14

---
+++ Semana 14/Veterinaria/Veterinaria/Entidades/Animal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Veterinaria.Entidades
+{
+    public class Animal
+    {
+        private int id;
+        private string nombre;
+        private float peso;
+        private List<Diagnostico> diagnosticos;
+
+        public Animal(int id, string nombre,float peso)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.peso = peso;
+            this.diagnosticos = new List<Diagnostico>();
+        }
+    }
+}
---
+++ Semana 14/Veterinaria/Veterinaria/Entidades/Diagnostico.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veterinaria.Entidades
+{
+    public class Diagnostico
+    {
+        private int id;
+        private string descripcion;
+        private DateTime fecha;
+        private float precio;
+
+        public Diagnostico(int id, string descripcion, DateTime fecha, float precio)
+        {
+            this.id = id;
+            this.descripcion = descripcion;
+            this.fecha = fecha;
+            this.precio = precio;
+        }
+    }
+}
